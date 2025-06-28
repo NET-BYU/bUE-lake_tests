34891e35fa763ba3ab57a654851122424f62e885
Ty Young <ty22117@byu.edu>
2025-06-27 23:38:58 -0600

    .

--- lora_tu_rd.py
+++ lora_tu_rd.py
@@ -61,7 +61,7 @@ for idx, params in enumerate(parameter_sets):
 
     tb.start()
 
-    timeout = 15 # seconds
+    timeout = 18 # seconds
     start_time = time.time()
     try:
         print(f'Running for up to {timeout} seconds. Press Ctrl+C to quit early.')
