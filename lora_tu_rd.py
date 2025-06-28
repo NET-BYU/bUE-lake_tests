c58c503b5b1ba6ca679894935608988d770a52b5
Eli Blattner <eli.blattner@icloud.com>
2025-06-27 23:29:21 -0600

    .

--- lora_tu_rd.py
+++ lora_tu_rd.py
@@ -61,7 +61,7 @@ for idx, params in enumerate(parameter_sets):
 
     tb.start()
 
-    timeout = 10  # seconds
+    timeout = 12 # seconds
     start_time = time.time()
     try:
         print(f'Running for up to {timeout} seconds. Press Ctrl+C to quit early.')
